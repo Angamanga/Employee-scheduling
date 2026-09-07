dbd225918824679a74af4cf07c044abb4a12bb28
Anna Iosif <anna@ushahidi.com>
2026-09-07 13:23:11 +0200

    Project setup #1

---
+++ readme.md
@@ -0,0 +1,13 @@
+#Employee scheduler
+
+To install:
+```npm install```
+
+To run:
+```npm run dev```
+
+To check for errors:
+```npm run lint```
+
+To fix errors:
+```npm run lint:fix```
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, this is the start of the Employee Scheduler!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
